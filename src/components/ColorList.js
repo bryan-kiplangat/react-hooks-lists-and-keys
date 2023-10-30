c91bdbc14b10a498a683829b8a77e6a51ed4b101
bryan-kiplangat <kipbryan.bett@gmail.com>
2023-10-30 08:12:57 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -10,11 +10,18 @@ function ColorList() {
     "green"
   ];
 
+  const colorElements = colors.map((color) => {
+    return <li style={{ color: color }}>{color}</li>
+  });
+
   return (
     <div>
       {/* have to manually change this */}
       <h1>Top 5 CSS Colors</h1>
       <ol>
+
+
+
         <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
@@ -22,7 +29,13 @@ function ColorList() {
         <li style={{ color: colors[4] }}>{colors[4]}</li>
         {/* hahve to manually change these values too. Posibility to mess up array numbering is high. */}
         <li style={{ color: colors[5] }}>{colors[5]}</li>
+
       </ol>
+
+      <h1>Top {colorElements.length} CSS Colors: Lists and Keys</h1>
+      {colorElements}
+
+
     </div>
   );
 }
