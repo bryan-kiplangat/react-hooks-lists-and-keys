949393fa08fd591bc8a5a7ae237f021d46903bd0
bryan-kiplangat <kipbryan.bett@gmail.com>
2023-10-30 07:45:23 +0300

    added another color

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,10 +7,12 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "green"
   ];
 
   return (
     <div>
+      {/* have to manually change this */}
       <h1>Top 5 CSS Colors</h1>
       <ol>
         <li style={{ color: colors[0] }}>{colors[0]}</li>
@@ -18,6 +20,8 @@ function ColorList() {
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
         <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {/* hahve to manually change these values too. Posibility to mess up array numbering is high. */}
+        <li style={{ color: colors[5] }}>{colors[5]}</li>
       </ol>
     </div>
   );
